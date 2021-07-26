6d551354a0491bbce5ea3e48ebe9fdfa9e8c1d05
Beata Zalewa <beata.zalewa@gmail.com>
2021-07-26 10:13:42 +0200

    Modified Startup class

--- AzuronetBees.Web/Startup.cs
+++ AzuronetBees.Web/Startup.cs
@@ -3,15 +3,15 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AzuronetBees.Web.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace AzuronetBees.Web
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -23,11 +23,16 @@ namespace AzuronetBees.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<BeeContext>(options =>
+           options.UseSqlite("Data Source=azuronetbees.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BeeContext beeContext)
         {
+            beeContext.Database.EnsureDeleted();
+            beeContext.Database.EnsureCreated();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
