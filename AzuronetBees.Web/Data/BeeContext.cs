3a7e027ac5f268f90e7e6c426b557dfec82bba32
Beata Zalewa <beata.zalewa@gmail.com>
2021-07-26 10:11:15 +0200

    Added BeeContext class

---
+++ AzuronetBees.Web/Data/BeeContext.cs
@@ -0,0 +1,16 @@
+﻿using AzuronetBees.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzuronetBees.Web.Data
+{
+    public class BeeContext : DbContext
+    {
+        public BeeContext(DbContextOptions<BeeContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Bee> Bees { get; set; }
+        public DbSet<Beehive> Beehives { get; set; }
+    }
+}
+
