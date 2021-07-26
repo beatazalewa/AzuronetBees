d96cd25775923915dea57c42b36d453341981ec1
Beata Zalewa <beata.zalewa@gmail.com>
2021-07-26 10:31:45 +0200

    Added IBeeRepository interface

---
+++ AzuronetBees.Web/Repositories/IBeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using AzuronetBees.Web.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AzuronetBees.Web.Repositories
+{
+    public interface IBeeRepository
+    {
+        IEnumerable<Bee> GetBees();
+        Bee GetBeeById(int id);
+        void CreateBee(Bee bee);
+        void DeleteBee(int id);
+        void SaveChanges();
+        IQueryable<Beehive> PopulateBeehivesDropDownList();
+    }
+}
+
